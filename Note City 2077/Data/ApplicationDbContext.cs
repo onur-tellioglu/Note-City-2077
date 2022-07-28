55ef979f318c6c4eecb3b9e6d0784ff06e3dbcc6
Onur Tellioğlu <94357256+onur-tellioglu@users.noreply.github.com>
2022-07-28 14:38:58 +0300

    Uploaded the project.

---
+++ Note City 2077/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NoteApp.Models;
+
+namespace NoteApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<NoteApp.Models.Note>? Note { get; set; }
+    }
+}
---
+++ Note City 2077/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿namespace NoteApp.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string NoteHeader { get; set; }
+        public string NoteBody { get; set; }
+        public Note()
+        {
+
+        }
+    }
+}
